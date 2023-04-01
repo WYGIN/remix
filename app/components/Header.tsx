e77e36e0ed5d763434fee4bc34058dd087218b0b
Sai Kiran Maggidi <107541780+WYGIN@users.noreply.github.com>
2023-04-01 17:38:30 +0530

    Create Header.tsx

---
+++ app/components/Header.tsx
@@ -0,0 +1,10 @@
+<header class="backdrop-blur bg-[#FFFFFFCC] h-[64px] border-b border-solid border-gray box-border text-[#2d3843] [color-scheme: light] flex flex-row shrink-0 text-base left-0 fixed right-0 [text-size-adjust: 100%] top-0 w-full [-webkit-font-smooting: antialiased] z-[1100] py-2 px-4">
+  <div class="items-center box-border text-[#2d3843] [color-scheme: light] flex text-base min-h-[48px] relative [text-size-adjust: 100%]">
+    <button class="items-center [appearance: none] bg-[#00000000] border border-[#e0e3e7] box-border [color-scheme: light] [cursor: pointer] flex justify-center p-2 ml-px relative [vertical-align: middle] h-[34px] w-[34px] rounded-[10px]"></button>
+    <a class="ml-4" href="">wygin</a>
+  </div>
+  <div class="flex-auto block"></div>
+  <div class="flex items-center justify-center">
+    <button class="ml-[10px] w-[34px] flex items-center justify-center h-[34px] border rounded-[10px] border-[#e0e3e7] box-border [color-scheme: light] relative [vertical-align: middle]"></button>
+  </div>
+</header>
