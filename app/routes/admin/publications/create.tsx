aed8f654068200a9ab2a6642a5fd84b63e77fb59
Sai Kiran Maggidi <107541780+WYGIN@users.noreply.github.com>
2023-04-01 18:52:43 +0530

    Update create.tsx

--- app/routes/admin/publications/create.tsx
+++ app/routes/admin/publications/create.tsx
@@ -1,1 +1,12 @@
+import { prisma } from '@utils/prisma';
+import { useLoaderData } from "@remix-run/react";
+
+
+
+
+export default function Page() {
+  const data = useLoaderData<typeof loader>();
+  return();
+}
+
 
