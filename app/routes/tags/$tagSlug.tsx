ae5c506204151c4c1b2630167e99513eeaf803f6
Sai Kiran Maggidi <107541780+WYGIN@users.noreply.github.com>
2023-04-01 18:36:56 +0530

    Update $tagSlug.tsx

--- app/routes/tags/$tagSlug.tsx
+++ app/routes/tags/$tagSlug.tsx
@@ -1,1 +1,18 @@
+import { prisma } from '@utils/prisma';
+import { useLoaderData } from "@remix-run/react";
+
+export const loader = async ({ params }: LoaderArgs) => {
+  const data = prisma.tag.findUnique({
+    where: {
+      slug: parama.tagSlug,
+    }
+  });
+  
+  return data;
+}
+
+export default function Tag() {
+  const data = useLoaderData<typeof loader>();
+  return();
+}
 
