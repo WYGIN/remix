f4262dbc9cd0fa987ca9b6014790cf5e83185f5a
Sai Kiran Maggidi <107541780+WYGIN@users.noreply.github.com>
2023-04-01 18:44:01 +0530

    Update $category2.tsx

--- app/routes/category/$category1/$category2.tsx
+++ app/routes/category/$category1/$category2.tsx
@@ -1,1 +1,19 @@
+import { prisma } from '@utils/prisma';
+import { useLoaderData } from "@remix-run/react";
+
+export const loader = async ({ params }: LoaderArgs) => {
+  const data = prisma.taxonomy.findUnique({
+    where: {
+      category1: parama.category1,
+      category1: params.category2,
+    }
+  });
+  
+  return data;
+}
+
+export default function Page() {
+  const data = useLoaderData<typeof loader>();
+  return();
+}
 
