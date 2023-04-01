43a20ebb021b1004bf4d1fdc3cf7df1f66d55f37
Sai Kiran Maggidi <107541780+WYGIN@users.noreply.github.com>
2023-04-01 19:30:24 +0530

    Update create.tsx

--- app/routes/admin/$publication/blog/create.tsx
+++ app/routes/admin/$publication/blog/create.tsx
@@ -1,1 +1,49 @@
+import { prisma } from '@utils/prisma';
+import { useLoaderData } from "@remix-run/react";
+
+export async function action({ request }: ActionArgs) {
+  const data = await request.formData();
+  const prisma = await prisma.publication.upsert({
+    where: {
+      slug: data.get('slug')
+    },
+    create: {
+      title: data.get('title'),
+      body: data.get('body'),
+      featuredImage: data.get('featuredImage'),
+      description: data.get('description'),
+      tags: data.get('tags'),
+      slug: data.get('slug'),
+      schema: data.get('schema'),
+      status: data.get('status'),
+      author: {
+        connect: {
+          id: data.get('authorId')
+        }
+      }
+    },
+    update: {
+      title: data.get('title'),
+      body: data.get('body'),
+      featuredImage: data.get('featuredImage'),
+      description: data.get('description'),
+      tags: data.get('tags'),
+      slug: data.get('slug'),
+      schema: data.get('schema'),
+      status: data.get('status'),
+      author: {
+        connect: {
+          id: data.get('authorId')
+        }
+      }
+    }
+  });
+}
+export default function Page() {
+  
+  return();
+}
+
+
+
 
