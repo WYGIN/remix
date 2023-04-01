f7809ed14c3282f4372823e68aad5d26d2f2c598
Sai Kiran Maggidi <107541780+WYGIN@users.noreply.github.com>
2023-04-01 17:39:49 +0530

    Create Drawer.tsx

---
+++ app/components/Drawer.tsx
@@ -0,0 +1,6 @@
+<nav id="drawer" class="fixed [inset: 0px] z-[1200] block text-[#1A2027]">
+  <div id="backdrop-overlay-aside" class="fixed flex items-center justify-center bg-[#00000080] z-[-1] opacity-100 transition-opacity duration-[225ms] ease-in-out delay-[0ms]"></div>
+  <div class="block"></div>
+  <div class="[transform: none] transition-transform duration-[225ms] ease-out sm:w-[310px] text-[#1A2027] bg-white flex flex-[1_0_auto] flex-col h-full z-[1200] fixed top-0 right-0 rounded-r-[10px] pb-[40px] [box-shadow: rgba(0, 0, 0, 0.2) 0px 8px 10px -5px, rgba(0, 0, 0, 0.14) 0px 16px 24px 2px, rgba(0, 0, 0, 0.12) 0px 6px 30px 5px]"></div>
+  <div class="block"></div>
+</nav>
