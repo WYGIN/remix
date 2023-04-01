aff8e1214ad4dba613a5a673cb648fe837b57acb
Sai Kiran Maggidi <107541780+WYGIN@users.noreply.github.com>
2023-04-01 17:39:07 +0530

    Create Sidebar.tsx

---
+++ app/components/Sidebar.tsx
@@ -0,0 +1,6 @@
+<aside class="fixed [inset: 0px] z-[1200] block text-[#1A2027]">
+  <div id="backdrop-overlay-aside" class="fixed flex items-center justify-center bg-[#00000080] z-[-1] opacity-100 transition-opacity duration-[225ms] ease-in-out delay-[0ms]"></div>
+  <div class="block"></div>
+  <div class="[transform: none] transition-transform duration-[225ms] ease-out sm:w-[310px] text-[#1A2027] bg-white flex flex-[1_0_auto] flex-col h-full z-[1200] fixed top-0 right-0 rounded-l-[10px]"></div>
+  <div class="block"></div>
+</aside>
