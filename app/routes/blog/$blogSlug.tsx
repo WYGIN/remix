e34d7e56f081b6d4244ff3daa1fd39fb0fb57367
Sai Kiran Maggidi <107541780+WYGIN@users.noreply.github.com>
2023-04-01 18:45:45 +0530

    Update $blogSlug.tsx

--- app/routes/blog/$blogSlug.tsx
+++ app/routes/blog/$blogSlug.tsx
@@ -1,1 +1,19 @@
+import { prisma } from '@utils/prisma';
+import { useLoaderData } from "@remix-run/react";
+
+export const loader = async ({ params }: LoaderArgs) => {
+  const data = prisma.taxonomy.findUnique({
+    where: {
+      slug: params.blogSlug,
+    }
+  });
+  
+  return data;
+}
+
+export default function Page() {
+  const data = useLoaderData<typeof loader>();
+  return();
+}
+
 
