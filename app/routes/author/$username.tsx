d5a78d3f80a4325b30f42e62c9faf91dc099904c
Sai Kiran Maggidi <107541780+WYGIN@users.noreply.github.com>
2023-04-01 18:48:23 +0530

    Update $username.tsx

--- app/routes/author/$username.tsx
+++ app/routes/author/$username.tsx
@@ -1,1 +1,19 @@
+import { prisma } from '@utils/prisma';
+import { useLoaderData } from "@remix-run/react";
+
+export const loader = async ({ params }: LoaderArgs) => {
+  const data = prisma.user.findUnique({
+    where: {
+      username: params.username,
+    }
+  });
+  
+  return data;
+}
+
+export default function Author() {
+  const data = useLoaderData<typeof loader>();
+  return();
+}
+
 
