d89c765e32fdd5bc8d1eabff92ffd366d8f4ae83
Sai Kiran Maggidi <107541780+WYGIN@users.noreply.github.com>
2023-04-02 14:17:51 +0530

    Update $postSlug.tsx

--- app/routes/$category1/$category2/$postSlug.tsx
+++ app/routes/$category1/$category2/$postSlug.tsx
@@ -1,1 +1,22 @@
+import { prisma } from '@utils/prisma';
+import { useLoaderData } from "@remix-run/react";
 
+export const loader = async ({ params }: LoaderArgs) => {
+  const data = prisma.post.findUnique({
+    where: {
+      taxonomy: {
+        category: {
+          category1: params.category1,
+          category2: params.category2,
+        },
+      },
+      slug: params.postSlug,
+    },
+  });
+
+  return data;
+}
+
+export default function Post() {
+  return();
+}
