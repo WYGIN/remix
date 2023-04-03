4c8a89a3411d22920f51d467352fb1a7274c60ec
Sai Kiran Maggidi <107541780+WYGIN@users.noreply.github.com>
2023-04-03 16:35:02 +0530

    Create Github.tsx

---
+++ app/scripts/Github.tsx
@@ -0,0 +1,37 @@
+import { Octokit } from '@octokit/core';
+
+const octokit = new Octokit({ 
+   auth: '' 
+ }); 
+  
+ export const gitput = async ({ buffer, width, height, username, email, filename, path }) => { 
+   const response = await octokit.request('PUT /repos/{owner}/{repo}/contents/{path}', { 
+     owner: 'OWNER', 
+     repo: 'REPO', 
+     path: path, 
+     message: `uploaded ${filename}`, 
+     committer: { 
+       name: username, 
+       email: email 
+     }, 
+     content: `{ 
+       data: ${buffer}, 
+       width: ${width}, 
+       height: ${height} 
+     }`, 
+   }); 
+   return `https://wygin.me/${response.content.path}`; 
+ } 
+  
+ export const gitget = async ({ path }) => { 
+   const response = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', { 
+     owner: 'OWNER', 
+     repo: 'REPO', 
+     path: path, 
+     headers: { 
+       'X-GitHub-Api-Version': '2022-11-28', 
+       'Accept': 'application/vnd.github.raw' 
+     } 
+   }); 
+   return response; 
+ } 
