b9a443a61e8e612e328152563bf7a7fcc6548448
Sai Kiran Maggidi <107541780+WYGIN@users.noreply.github.com>
2023-04-03 11:13:50 +0530

    Update Sidebar.tsx

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,6 +1,6 @@
 export default function Sidebar() {
   return(
-      <aside className="fixed [inset: 0px] z-[1200] block text-[#1A2027]">
+      <aside className="fixed [inset: 0px] z-[1200] block text-[#1A2027] hidden md:block">
         <div id="backdrop-overlay-aside" className="fixed flex items-center justify-center bg-[#00000080] z-[-1] opacity-100 transition-opacity duration-[225ms] ease-in-out delay-[0ms]"></div>
         <div className="block"></div>
         <div className="[transform: none] transition-transform duration-[225ms] ease-out sm:w-[310px] text-[#1A2027] bg-white flex flex-[1_0_auto] flex-col h-full z-[1200] fixed top-0 right-0 rounded-l-[10px]">
