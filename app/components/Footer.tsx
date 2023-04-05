4445b960e133fa80397a976e5b29a44f39b77c6d
Sai Kiran Maggidi <107541780+WYGIN@users.noreply.github.com>
2023-04-05 13:32:01 +0530

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -31,8 +31,10 @@ export default function Footer({footer}) {
                   <div>
                   <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white" key={index}>{item.label}</h2>
                   <ul className="text-gray-600 dark:text-gray-400 font-medium">
-                    { item.items.map((subitem, index) => (
-                      { items(item ,subitem, index) }
+                    { item.items.map((subitem, index) => { 
+                        items(item ,subitem, index); 
+                      }
+                    )}
                   </ul>
                   </div>
                 )}
