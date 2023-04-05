5efdc9f2d104d2e5938f35c9c2ca4aa1e0e9b387
Sai Kiran Maggidi <107541780+WYGIN@users.noreply.github.com>
2023-04-05 18:07:02 +0530

    Update Github.tsx

--- app/scripts/Github.tsx
+++ app/scripts/Github.tsx
@@ -1,37 +1,11 @@
-import { Octokit } from '@octokit/core';
-
-const octokit = new Octokit({ 
-   auth: '' 
- }); 
+import { gitPut, gitGet } from '~/utils/github.server';
   
  export const githubPut = async ({ buffer, width, height, username, email, filename, path }) => { 
-   const response = await octokit.request('PUT /repos/{owner}/{repo}/contents/{path}', { 
-     owner: 'OWNER', 
-     repo: 'REPO', 
-     path: path, 
-     message: `uploaded ${filename}`, 
-     committer: { 
-       name: username, 
-       email: email 
-     }, 
-     content: `{ 
-       data: ${buffer}, 
-       width: ${width}, 
-       height: ${height} 
-     }`, 
-   }); 
+   const response = await gitPut(buffer, width, height, username, email, filename, path);
    return `https://wygin.me/${response.content.path}`; 
  } 
   
  export const githubGet = async ({ path }) => { 
-   const response = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', { 
-     owner: 'OWNER', 
-     repo: 'REPO', 
-     path: path, 
-     headers: { 
-       'X-GitHub-Api-Version': '2022-11-28', 
-       'Accept': 'application/vnd.github.raw' 
-     } 
-   }); 
+   const response = await gitGet(path);
    return response; 
  } 
