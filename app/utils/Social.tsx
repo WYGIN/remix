86d36a50f44b6be8cf0c7415bfc344fa82638c29
Sai Kiran Maggidi <107541780+WYGIN@users.noreply.github.com>
2023-04-05 14:00:27 +0530

    Update Social.tsx

--- app/utils/Social.tsx
+++ app/utils/Social.tsx
@@ -1,7 +1,31 @@
-export const getAccountFromSocial = (platform, username) => {
-  
+import { prisma } from '~/utils/prisma.server';
+
+enum Platform {
+  YOUTUBE 
+  INSTAGRAM 
+  GITHUB 
+  FACEBOOK 
+  TWITTER
+}
+
+export const getAccountFromSocial = async (platform, username) => {
+  const data = prisma.socialAccount.findUnique({
+    where: {
+      socialPlatform: platform,
+      username: username
+    }
+  });
 }
 
 export const getAccountNameFromSocial = (platform) => {
-  
+  if(platform === Platform.YOUTUBE) 
+    return "YouTube Logo";
+  if(platform === Platform.INSTAGRAM)
+    return "Instagram Logo";
+  if(platform === Platform.GITHUB)
+    return "Github Logo";
+  if(platform === Platform.FACEBOOK)
+    return "Facebook Logo";
+  if(platform === Platform.TWITTER)
+    return "Twitter Logo";
 }
